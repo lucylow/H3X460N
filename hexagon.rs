0371841bfce35083b012c8faadeea687fc909ae9
盧露 <low.lucyy@gmail.com>
2023-02-23 22:21:06 -0500

    Update hexagon.rs

--- hexagon.rs
+++ hexagon.rs
@@ -60,4 +60,111 @@ fn neural_network(input: &ArrayD<f32>) -> ArrayD<f32> {
     let w1 = Array::random((CONV1_FILTERS, INPUT_SHAPE[1], CONV1_KERNEL_SIZE[0], CONV1_KERNEL_SIZE[1]), rng);
     x = x.dot(&w1).into_dyn().into_dimensionality::<ndarray::Ix4>().unwrap();
     x = x.map(|x| if x < 0.0 { 0.0 } else { x });
-    let w2 = Array::random((CONV2_FILTERS, CONV1_FILTERS, CONV2_KERNEL_SIZE[0
+    let w2 = Array::random((CONV2_FILTERS, CONV1_FILTERS, CONV2_KERNEL_SIZE[0]
+        
+use std::collections::HashMap;
+
+struct Board {
+    // Your board struct here
+}
+
+#[derive(Copy, Clone)]
+enum Player {
+    X,
+    O,
+}
+
+impl Player {
+    fn opposite(&self) -> Player {
+        match self {
+            Player::X => Player::O,
+            Player::O => Player::X,
+        }
+    }
+}
+
+struct GameState {
+    board: Board,
+    current_player: Player,
+}
+
+struct Node {
+    state: GameState,
+    parent: Option<usize>,
+    children: Vec<usize>,
+    visits: f32,
+    total_reward: f32,
+}
+
+impl Node {
+    fn new(state: GameState, parent: Option<usize>) -> Node {
+        Node {
+            state,
+            parent,
+            children: vec![],
+            visits: 0.0,
+            total_reward: 0.0,
+        }
+    }
+
+    fn uct_score(&self, exploration: f32) -> f32 {
+        if self.visits == 0.0 {
+            return std::f32::INFINITY;
+        }
+        let exploitation = self.total_reward / self.visits;
+        let uct = exploration * (self.parent.unwrap().visits.ln() / self.visits).sqrt();
+        exploitation + uct
+    }
+}
+
+fn uct_search(state: GameState, max_iterations: usize, exploration: f32) -> usize {
+    let mut nodes = vec![Node::new(state, None)];
+    let mut rng = rand::thread_rng();
+    for _ in 0..max_iterations {
+        let mut node_idx = 0;
+        while !nodes[node_idx].children.is_empty() {
+            let best_child_idx = nodes[node_idx]
+                .children
+                .iter()
+                .max_by(|&a, &b| {
+                    nodes[*a]
+                        .uct_score(exploration)
+                        .partial_cmp(&nodes[*b].uct_score(exploration))
+                        .unwrap()
+                })
+                .unwrap();
+            node_idx = *best_child_idx;
+        }
+        let new_state = // Generate new state from current state
+            let moves = new_state.board.get_possible_moves();
+        let new_node_idx = nodes.len();
+        nodes.push(Node::new(new_state, Some(node_idx)));
+        nodes[node_idx].children.push(new_node_idx);
+        let reward = simulate_game(nodes[new_node_idx].state, &moves, &mut rng);
+        let mut backprop_idx = Some(new_node_idx);
+        while let Some(idx) = backprop_idx {
+            nodes[idx].visits += 1.0;
+            nodes[idx].total_reward += reward;
+            backprop_idx = nodes[idx].parent;
+        }
+    }
+    let best_child_idx = nodes[0]
+        .children
+        .iter()
+        .max_by(|&a, &b| nodes[*a].visits.partial_cmp(&nodes[*b].visits).unwrap())
+        .unwrap();
+    *best_child_idx
+}
+
+fn simulate_game(state: GameState, moves: &[(usize, usize)], rng: &mut impl rand::Rng) -> f32 {
+    let mut current_state = state;
+    let mut current_player = current_state.current_player;
+    let mut winner = None;
+    while winner.is_none() {
+        let (i, j) = moves[rng.gen_range(0..moves.len())];
+        current_state.board.set_cell(i, j, current_player);
+        winner = current_state.board.get_winner();
+        current_player = current_player.opposite();
+    }
+
+
